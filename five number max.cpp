f11fbfce3141ad645f4442ea8523465fe061e077
mahsa1994-7 <mahsanoor85@gmail.com>
2024-12-06 20:21:41 +0330

    my first commit for uni project

---
+++ five number max.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    int maximum;
+    maximum=0
+
+    ;cout<<"enter the five numbs:";
+  for(int i=1; i<=5;i++)
+   cin>>num;
+  if(i==1 || i>maximum )
+   maximum=num;
+
+cout<<" max is"<<maxm;
+}
